525b1976a79198809998ee9dc5b06c1ce0a9b797
hamiltontech <operations@hst.jo>
2023-11-30 11:35:55 +0300

    video background

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,43 @@ const Home = ({ frontmatter }) => {
 
   return (
     <Base title={title}>
+
+
+
+
+<main className="w-full h-[100vh]">
+<video
+        autoPlay
+        loop
+        muted
+        className=" w-full object-cover h-full"
+        src="/hero.mp4"
+      />
+<div className="absolute w-full top-0">
+<div className=" w-full m-0 p-0 bg-black/40 h-[100vh]">
+    <div class="grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12 pt-[300px] md:pt-[300px]">
+<div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Emdad Digital Platform</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Digitally Transforming the B2B Procurement Experience!</p>
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get started
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <a href="#" class=" inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-full hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Speak to Sales
+            </a> 
+        </div>
+        </div>
+        </div>
+</div>
+</main>
+     
+
+
+
       {/* Banner */}
-      <section class="bg-white dark:bg-gray-900 section bg-cover bg-fixed h-[750px]" style={{backgroundImage: 'url("https://i.imgur.com/ATdkAwx.jpg")'}}>
-        
-        <div className="w-full m-0 p-0 bg-black/30 h-[750px]">
+      {/* <section class="bg-white dark:bg-gray-900 section bg-cover bg-fixed h-[750px]" style={{backgroundImage: 'url("https://i.imgur.com/ATdkAwx.jpg")'}}>
+      <div className=" w-full m-0 p-0 bg-black/30 h-[750px]">
     <div class="grid max-w-screen-xl px-4  mx-auto lg:gap-8 xl:gap-0  lg:grid-cols-12 pt-[200px] md:pt-[200px]">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Emdad Digital Platform</h1>
@@ -35,11 +68,11 @@ const Home = ({ frontmatter }) => {
             </a> 
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" /> */}
+            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
         </div>                
     </div>
     </div>
-</section>
+</section> */}
 
       {/* Features */}
       <section className="section bg-body">
